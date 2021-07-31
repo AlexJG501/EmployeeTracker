9376dc332739f708fd0faa2ebc9db39c109bc22a
Alex Godwin <alexj.godwin@gmail.com>
2021-07-31 11:36:24 -0400

    update db and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,33 @@
+use employee_db;
+
+INSERT INTO department (name)
+VALUES 
+    ('Sales'),
+    ('Engineering'),
+    ('Marketing'),
+    ('Legal'),
+    ('Finance');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ('Software Engineer', 120000, 2),
+    ('Engineering Manager', 210000, 2),
+    ('Salesperson', 75000, 1),
+    ('Sales Manager', 950000, 1),
+    ('Marketing Manager', 130000, 3),
+    ('Marketing Associate', 90000, 3),
+    ('Attorney', 150000, 4),
+    ('Paralegal', 65000, 4),
+    ('Financial Analyst', 88000, 5),
+    ('Finance Manager', 140000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Jerry', 'Smith', 1, 2),
+    ('Tom', 'Brady', 2, NULL),
+    ('Michael', 'Jordan', 3, 4),
+    ('Scooby', 'Doo', 4, NULL),
+    ('Patrick', 'Star', 5, 4),
+    ('Stevie', 'Nicks', 6, 5),
+    ('Elton', 'John', 7, 2),
+    ('Harry', 'Potter', 8, NULL);
